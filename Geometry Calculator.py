7f376a402d857c3982ad715ccc19b99869366266
Louie Jay Oga <kmshinichi@gmail.com>
2024-11-01 09:52:33 +0800

    Geometry Calculator.py

--- Geometry Calculator.py
+++ Geometry Calculator.py
@@ -13,15 +13,7 @@ if measure == "area":
     shape = input("What shape do you wish to solve? ").lower()
     if shape == "square":
         unit = input("What unit of measurement is being asked? ").lower()
-        if unit == "cm":
-            side = float(input("What is the side length of the square? "))
-            area = side**2
-            print(f"The area of the {shape} is {round(area, 2)}{unit}².")
-        elif unit == "km":
-            side = float(input("What is the side length of the square? "))
-            area = side**2
-            print(f"The area of the {shape} is {round(area, 2)}{unit}².")
-        elif unit == "m":
+        if unit == "cm" or "km" or "m":
             side = float(input("What is the side length of the square? "))
             area = side**2
             print(f"The area of the {shape} is {round(area, 2)}{unit}².")
@@ -30,17 +22,7 @@ if measure == "area":
 
     elif shape == "rectangle":
         unit = input("What unit of measurement is being asked? ").lower()
-        if unit == "cm":
-            length = float(input("What is the length of the rectangle? "))
-            width = float(input("What is the width of the rectangle? "))
-            area = length * width
-            print(f"The area of the {shape} is {round(area, 2)}{unit}².")
-        elif unit == "km":
-            length = float(input("What is the length of the rectangle? "))
-            width = float(input("What is the width of the rectangle? "))
-            area = length * width
-            print(f"The area of the {shape} is {round(area, 2)}{unit}².")
-        elif unit == "m":
+        if unit == "cm" or "km" or "m":
             length = float(input("What is the length of the rectangle? "))
             width = float(input("What is the width of the rectangle? "))
             area = length * width
@@ -50,15 +32,7 @@ if measure == "area":
 
     elif shape == "circle":
         unit = input("What unit of measurement is being asked? ").lower()
-        if unit == "cm":
-            radius = float(input("What is the radius of the circle? "))
-            area = math.pi * radius**2
-            print(f"The area of the {shape} is {round(area, 2)}{unit}²")
-        elif unit == "in":
-            radius = float(input("What is the radius of the circle? "))
-            area = math.pi * radius**2
-            print(f"The area of the {shape} is {round(area, 2)}{unit}²")
-        elif unit == "m":
+        if unit == "cm" or "in" or "m":
             radius = float(input("What is the radius of the circle? "))
             area = math.pi * radius**2
             print(f"The area of the {shape} is {round(area, 2)}{unit}²")
@@ -67,17 +41,7 @@ if measure == "area":
 
     elif shape == "triangle":
         unit = input("What unit of measurement is being asked? ").lower()
-        if unit == "cm":
-            base = float(input("What is the base of the triangle? "))
-            height = float(input("What is the height of the triangle? "))
-            area = base * height / 2
-            print(f"The area of the {shape} is {round(area, 2)}{unit}²")
-        elif unit == "m":
-            base = float(input("What is the base of the triangle? "))
-            height = float(input("What is the height of the triangle? "))
-            area = base * height / 2
-            print(f"The area of the {shape} is {round(area, 2)}{unit}²")
-        elif unit == "in":
+        if unit == "cm" or "in" or "m":
             base = float(input("What is the base of the triangle? "))
             height = float(input("What is the height of the triangle? "))
             area = base * height / 2
@@ -93,15 +57,7 @@ elif measure == "perimeter":
     shape = input("What shape do you wish to solve? ").lower()
     if shape == "square":
         unit = input("What unit of measurement is being asked? ").lower()
-        if unit == "cm":
-            side = float(input("What is the side length of the square? "))
-            perimeter = 4 * side
-            print(f"The perimeter of the square is {round(perimeter, 2)}{unit}")
-        elif unit == "km":
-            side = float(input("What is the side length of the square? "))
-            perimeter = 4 * side
-            print(f"The perimeter of the square is {round(perimeter, 2)}{unit}")
-        elif unit == "m":
+        if unit == "cm" or "km" or "m":
             side = float(input("What is the side length of the square? "))
             perimeter = 4 * side
             print(f"The perimeter of the square is {round(perimeter, 2)}{unit}")
@@ -110,17 +66,7 @@ elif measure == "perimeter":
 
     elif shape == "rectangle":
         unit = input("What unit of measurement is being asked? ").lower()
-        if unit == "cm":
-            length = float(input("What is the length of the rectangle? "))
-            width = float(input("What is the width of the rectangle? "))
-            perimeter = 2 * length + 2 * width
-            print(f"The perimeter of the square is {round(perimeter, 2)}{unit}")
-        elif unit == "km":
-            length = float(input("What is the length of the rectangle? "))
-            width = float(input("What is the width of the rectangle? "))
-            perimeter = 2 * length + 2 * width
-            print(f"The perimeter of the square is {round(perimeter, 2)}{unit}")
-        elif unit == "m":
+        if unit == "cm" or "km" or "m":
             length = float(input("What is the length of the rectangle? "))
             width = float(input("What is the width of the rectangle? "))
             perimeter = 2 * length + 2 * width
@@ -130,15 +76,7 @@ elif measure == "perimeter":
 
     elif shape == "circle":
         unit = input("What unit of measurement is being asked? ").lower()
-        if unit == "cm":
-            radius = float(input("What is the radius of the circle? "))
-            perimeter = 2 * math.pi * radius
-            print(f"The perimeter of the {shape} is {round(perimeter, 2)}{unit}")
-        elif unit == "in":
-            radius = float(input("What is the radius of the circle? "))
-            perimeter = 2 * math.pi * radius
-            print(f"The perimeter of the {shape} is {round(perimeter, 2)}{unit}")
-        elif unit == "m":
+        if unit == "cm" or "in" or "m":
             radius = float(input("What is the radius of the circle? "))
             perimeter = 2 * math.pi * radius
             print(f"The perimeter of the {shape} is {round(perimeter, 2)}{unit}")
@@ -147,21 +85,7 @@ elif measure == "perimeter":
 
     elif shape == "triangle":
         unit = input("What unit of measurement is being asked? ").lower()
-        if unit == "cm":
-            a = float(input("What is the measurement of the right side of the triangle? "))
-            b = float(input("What is the measurement of the left side of the triangle? "))
-            c = float(input("What is the measurement of the base of the triangle? "))
-            perimeter = a + b + c
-            print(f"The perimeter of the {shape} is {round(perimeter, 2)}{unit}")
-
-        elif unit == "in":
-            a = float(input("What is the measurement of the right side of the triangle? "))
-            b = float(input("What is the measurement of the left side of the triangle? "))
-            c = float(input("What is the measurement of the base of the triangle? "))
-            perimeter = a + b + c
-            print(f"The perimeter of the {shape} is {round(perimeter, 2)}{unit}")
-
-        elif unit == "m":
+        if unit == "cm" or "in" or "m":
             a = float(input("What is the measurement of the right side of the triangle? "))
             b = float(input("What is the measurement of the left side of the triangle? "))
             c = float(input("What is the measurement of the base of the triangle? "))
@@ -171,21 +95,14 @@ elif measure == "perimeter":
             print("Invalid")
     else:
         print("Invalid")
+        
 #Volume
 
 elif measure == "volume":
     shape = input("What shape do you wish to solve? ").lower()
     if shape == "cube":
         unit = input("What unit of measurement is being asked? ").lower()
-        if unit == "cm":
-            side = float(input("What is the side of the cube? "))
-            volume = side**3
-            print(f"The volume of the {shape} is {round(volume, 2)}{unit}³")
-        elif unit == "m":
-            side = float(input("What is the side of the cube? "))
-            volume = side**3
-            print(f"The volume of the {shape} is {round(volume, 2)}{unit}³")
-        elif unit == "in":
+        if unit == "cm" or "in" or "m":
             side = float(input("What is the side of the cube? "))
             volume = side**3
             print(f"The volume of the {shape} is {round(volume, 2)}{unit}³")
@@ -194,19 +111,7 @@ elif measure == "volume":
 
     elif shape == "cuboid":
         unit = input("What unit of measurement is being asked? ").lower()
-        if unit == "cm":
-            length = float(input("What is the length of the cuboid? "))
-            breadth = float(input("What is the breadth of the cuboid? "))
-            height = float(input("What is the height of the  cuboid? "))
-            volume = length * breadth * height
-            print(f"The volume of the {shape} is {round(volume, 2)}{unit}³")
-        elif unit == "m":
-            length = float(input("What is the length of the cuboid? "))
-            breadth = float(input("What is the breadth of the cuboid? "))
-            height = float(input("What is the height of the  cuboid? "))
-            volume = length * breadth * height
-            print(f"The volume of the {shape} is {round(volume, 2)}{unit}³")
-        elif unit == "in":
+        if unit == "cm" or "in" or "m":
             length = float(input("What is the length of the cuboid? "))
             breadth = float(input("What is the breadth of the cuboid? "))
             height = float(input("What is the height of the  cuboid? "))
@@ -217,15 +122,7 @@ elif measure == "volume":
 
     elif shape == "sphere":
         unit = input("What unit of measurement is being asked? ").lower()
-        if unit == "cm":
-            radius = float(input("What is the radius of the sphere? "))
-            volume = 4 * math.pi * radius**3 / 3
-            print(f"The volume of the {shape} is {round(volume, 2)}{unit}³")
-        elif unit == "m":
-            radius = float(input("What is the radius of the sphere? "))
-            volume = 4 * math.pi * radius**3 / 3
-            print(f"The volume of the {shape} is {round(volume, 2)}{unit}³")
-        elif unit == "in":
+        if unit == "cm" or "in" or "m":
             radius = float(input("What is the radius of the sphere? "))
             volume = 4 * math.pi * radius**3 / 3
             print(f"The volume of the {shape} is {round(volume, 2)}{unit}³")
@@ -234,19 +131,7 @@ elif measure == "volume":
 
     elif shape == "triangular prism":
         unit = input("What unit of measurement is being asked? ").lower()
-        if unit == "cm":
-            base = float(input("What is the base of the triangular prism? "))
-            height = float(input("What is the height of the triangular prism? "))
-            length = float(input("What is the length of the triangular prism? "))
-            volume = base * height * length / 2
-            print(f"The volume of the {shape} is {round(volume, 2)}{unit}³")
-        elif unit == "m":
-            base = float(input("What is the base of the triangular prism? "))
-            height = float(input("What is the height of the triangular prism? "))
-            length = float(input("What is the length of the triangular prism? "))
-            volume = base * height * length / 2
-            print(f"The volume of the {shape} is {round(volume, 2)}{unit}³")
-        elif unit == "in":
+        if unit == "cm" or "in" or "m":
             base = float(input("What is the base of the triangular prism? "))
             height = float(input("What is the height of the triangular prism? "))
             length = float(input("What is the length of the triangular prism? "))
